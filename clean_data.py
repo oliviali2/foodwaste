3369b4c9de05c61983c0eef1dd09077deedc2b37
Ingrid Fan <ingridyfan@gmail.com>
2018-11-13 21:46:50 -0800

    first file

---
+++ clean_data.py
@@ -0,0 +1,24 @@
+import numpy as py
+import sys
+import numpy as np
+import pandas as pd
+import itertools
+# import matplotlib
+# import collections
+# import random
+# import operator
+# import timei
+
+def main(): 
+	print "start"
+	inputfilename = "train.csv"
+	data = pd.read_csv(inputfilename)
+
+	
+	print "finished"
+
+
+
+
+if __name__ == '__main__':
+    main()
